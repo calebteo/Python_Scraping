bf2202a0bc0c835c4eb00663632baa2e5975e26e
Caleb Teo <calebteo@Calebs-MacBook-Pro.local>
2019-01-23 17:03:38 +0000

    Complete Extraction of first 10 pokemon data

--- scrape_pokemon.py
+++ scrape_pokemon.py
@@ -1,33 +1,49 @@
 
 import requests
+import time
 from bs4 import BeautifulSoup
 
 
 def main():
     print("Initialise Request")
-    result = request_result("https://www.pokemon.com/us/pokedex/")
+    base_link = "https://www.pokemon.com/us/pokedex"
+    pokemon_list = []
+    result = request_result(base_link)
 
     if result[0] == 200:
-        print("Extracting Data")
-        # Getting Pokemon Information
+        # Getting a list of links of pokemons
         soup = BeautifulSoup(result[1], 'lxml')
-        #pokemon_urls = soup.find('section', {'class' : 'section pokedex-results overflow-visible'})
         pokemon_urls = []
         links = []
-        li = soup.find_all('li')
 
-        for i in range(len(li)):
-            a = li[i].find('a')
+        # Finding all 'li tags' that contain 'a tags' with pokemon links in format /us/pokedex/<pokemon name>
+        li = soup.find_all('li')
+        for li_tags in li:
+            a = li_tags.find('a')
             if a is not None and 'href' in a.attrs:
-                link = li[i].find('a').attrs['href']
+                link = a.attrs['href']
                 links.append(link)
 
+        # From all links extract links with pokedex only i.e. it is a valid link to a pokemon entry
         for link in links:
             if link.find('pokedex') >= 0:
                 pokemon_urls.append(link)
 
         print(pokemon_urls)
         print(len(pokemon_urls))
+
+        # Extract pokemon information from each page with 2 second delay between each entry for safety
+        for i in range(1, 10):
+            url = "https://www.pokemon.com"+pokemon_urls[i]
+            print(url)
+            pokemon_results = request_result(url)
+
+            if pokemon_results[0] == 200:
+                pokemon_soup = BeautifulSoup(pokemon_results[1], 'lxml')
+                pokemon_list.append(extract_pokemon_data(pokemon_soup))
+                time.sleep(2)
+
+        print(pokemon_list)
 
     else:
         print("Error Status Code: " + str(result[0]))
