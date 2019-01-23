dc990081f35c32dc8e2635b027f4973f6bd1ca2b
Caleb Teo <calebteo@Calebs-MacBook-Pro.local>
2019-01-23 14:57:58 +0000

    Updated functions

--- scrape_pokemon.py
+++ scrape_pokemon.py
@@ -2,45 +2,48 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def main():
     print("Initalise Request")
-    result = request_result("us/pokedex/bulbasaur")
+    result = request_result("https://www.pokemon.com/us/pokedex/bulbasaur")
 
-    if result[0] == 200 :
+    if result[0] == 200:
         print("Extracting Data")
         # Getting Pokemon Information
         soup = BeautifulSoup(result[1], 'lxml')
         pokemonlist = extract_pokemon_data(soup)
         print(pokemonlist)
 
-    else :
+    else:
         print("Error Status Code: " + str(result[0]))
 
+
 def request_result(url):
-    #Access Pokemon Page
-    page = requests.get("https://www.pokemon.com/" + url)
+    # Access Pokemon Page
+    page = requests.get(url)
     print('Status Code: ' + str(page.status_code))
     result = [page.status_code, page.content]
     return result
 
 
-def extract_pokemon_data(soup) :
-    #Getting Pokemon Name
+def extract_pokemon_data(soup):
+    # Getting Pokemon Name
     div_name = soup.find('div', {'class' : 'pokedex-pokemon-pagination-title'})
-    #print(div_name)
+    # print(div_name)
 
     name = div_name.text.strip()
     name = name.replace(" ","")
     name_number = name.split("\n")
-    #print(name_number)
+    # print(name_number)
 
-    #Getting Pokemon Type
+    # Getting Pokemon Type
     div_type = soup.find('div', {'class' : 'dtm-type'})
     types = div_type.text
     types = types.split("\n")
     types = list(filter(None, types))
-    #print(types)
+    # print(types)
 
     return name_number + types
+
 
 main()
